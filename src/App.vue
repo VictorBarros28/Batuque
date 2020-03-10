0baee72820ea1ff4ca78728e519a3fbef156edd1
Daniel Lima <danielsalgueirolima@gmail.com>
2020-03-10 17:30:41 -0300

    carrossel

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,111 @@
 <template>
-  <div class="container">
-  sasa
-    <div class="row justify-content-center">
-      <div class="col-md-8">
-        <h2><router-link to="/index">Subscriptions</router-link></h2>
-        <router-link to="/create" class="btn btn-sm btn-primary">Add subscription</router-link>
-              
-        <router-view />
-      </div>
-    </div>
-  </div>
+  <div class="banner">
+    
+  <md-card>
+    <md-card-actions>
+      
+      
+    </md-card-actions>
+    <md-card-media>
+      <!-- swiper -->
+      <swiper :options="swiperOption">
+        
+        <swiper-slide class="slides">
+          <p class="texto">"Lorem ipsum dolor sit amet, consectetur adipiscing 
+            elit, sed do eiusmod tempor incididunt ut labore et 
+            dolore magna aliqua. Ut enim ad minim veniam, 
+            quis nostrud exercitation ullamco laboris nisi ut 
+            aliquip ex ea commodo consequat.</p>
+          <img class="imagens" src="./assets/sobre.png" alt="">
+        </swiper-slide>
+        <swiper-slide class="slides">
+          <p class="texto">"Lorem ipsum dolor sit amet, consectetur adipiscing 
+            elit, sed do eiusmod tempor incididunt ut labore et 
+            dolore magna aliqua. Ut enim ad minim veniam, 
+            quis nostrud exercitation ullamco laboris nisi ut 
+            aliquip ex ea commodo consequat.</p>
+          <img class="imagens" src="./assets/sobre_dois.png" alt="">
+        </swiper-slide>
+        <swiper-slide class="slides">
+          <p class="texto">"Lorem ipsum dolor sit amet, consectetur adipiscing 
+            elit, sed do eiusmod tempor incididunt ut labore et 
+            dolore magna aliqua. Ut enim ad minim veniam, 
+            quis nostrud exercitation ullamco laboris nisi ut 
+            aliquip ex ea commodo consequat.</p>
+          <img class="imagens" src="./assets/sobre_tres.png" alt="">
+        </swiper-slide>
+        <swiper-slide class="slides">
+          <p class="texto">"Lorem ipsum dolor sit amet, consectetur adipiscing 
+            elit, sed do eiusmod tempor incididunt ut labore et 
+            dolore magna aliqua. Ut enim ad minim veniam, 
+            quis nostrud exercitation ullamco laboris nisi ut 
+            aliquip ex ea commodo consequat.</p>
+          <img class="imagens" src="./assets/sobre_quatro.png" alt="">
+        </swiper-slide>
+       
+        
+        <div class="swiper-pagination" slot="pagination"></div>
+      </swiper>
+    </md-card-media>
+  </md-card>
+</div>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        swiperOption: {
+          loop:true,
+          pagination: {
+            el: '.swiper-pagination',
+            
+          }
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+
+
+.slides{
+  
+  width: 100%;
+  height: 0%;
+  background-color: rgba(255, 255, 255, 0.750);
+  display: flex;
+  justify-content: center;  
+  justify-content: space-between;
+  padding-left: 15%;
+  padding-right: 15%;
+  align-items: center;
+}
+.banner{
+  background-image: url('./assets/banner.png');
+  width: 100%;
+  height: 100vh;
+  background-position: right;
+  background-size: cover;
+}
+.swiper-slide{
+  height: 100vh !important;
+}
+.md-card-actions{
+  padding: 0px !important;
+}
+.imagens{
+  width: 350px !important;
+  height: 350px !important;
+  
+}
+.texto{
+  width: 540px;
+  height:120px;
+  text-align: center;
+  font-family: Calibri;
+  color: #707070;
+  font-size: 25px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -4,12 +4,20 @@ import router from "./router";
 import store from "./store";
 import BootstrapVue from "bootstrap-vue";
 import VeeValidate from 'vee-validate';
+import VueAwesomeSwiper from 'vue-awesome-swiper';
 
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap-vue/dist/bootstrap-vue.css"
+import VueMaterial from 'vue-material';
+import 'vue-material/dist/vue-material.css'; 
 
-Vue.use(BootstrapVue)
+
+import 'swiper/dist/css/swiper.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+Vue.use(BootstrapVue);
 Vue.use(VeeValidate);
+Vue.use(VueAwesomeSwiper)
+Vue.use(VueMaterial)
 
 Vue.config.productionTip = false;
 
