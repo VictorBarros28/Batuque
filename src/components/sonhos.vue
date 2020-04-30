00d67c2630b7f6aa6c00e0709becde74577b38a4
VictorBarros28 <53492984+VictorBarros28@users.noreply.github.com>
2020-04-30 02:52:25 -0300

    modais colocados

--- src/components/sonhos.vue
+++ src/components/sonhos.vue
@@ -2,124 +2,264 @@
 
 
 <div class="title">
-<h1> SONHOS ENTREGUES</h1>
+<h1> SONHOS ENTREGUES </h1>
+<br>
+<br>
+<!-- TODO : Conseguir forma de manipular "title, para que o responsivo funcione bem e a navbar não seja afetada." -->
+
    <div class = "portfolio">
   <b-card  class = "tabs" no-body>
     <b-tabs card>
-      <b-tab title="IDENTIDADE VISUAL" active>
-         <div class = "nomes"> 
+      <b-tab title="IDENTIDADE VISUAL⠀⠀⠀⠀⠀" title-link-class ="h4" active> 
+         <div class = "nomes">
      <div class = "proj">
-      <img src= "../assets/Imagem 8.png"  href="" class="img-responsive">
-      <img src= "../assets/Imagem 4.png"  href="" class="img-responsive">
-      <img src= "../assets/Imagem 6.png"  href="" class="img-responsive">
-      <img src= "../assets/Imagem 7.png"  href="" class="img-responsive">
-      <img src= "../assets/Imagem 11.png" href="" class="img-responsive">
-      <div class = "correcao">
-      <img src= "../assets/Imagem 10.png" href="" class="img-r1">
+      <img src= "../assets/Imagem 8.png"  v-b-modal.modal-center1 href="" class="img-responsive">
+        <b-modal id="modal-center1" centered title="BootstrapVue">
+    <p class="my-4">Modal 1!</p>
+  </b-modal>
+      <img src= "../assets/Imagem 4.png" v-b-modal.modal-center2 href="" class="img-responsive">
+              <b-modal id="modal-center2" centered title="BootstrapVue">
+    <p class="my-4">Modal 2!</p>
+  </b-modal>
+      <img src= "../assets/Imagem 6.png" v-b-modal.modal-center3 href="" class="img-responsive">
+              <b-modal id="modal-center3" centered title="BootstrapVue">
+    <p class="my-4">Modal 3!</p>
+  </b-modal>
+      <img src= "../assets/Imagem 7.png" v-b-modal.modal-center4 href="" class="img-responsive">
+              <b-modal id="modal-center4" centered title="BootstrapVue">
+    <p class="my-4">Modal 4!</p>
+  </b-modal>
+      <img src= "../assets/Imagem 11.png" v-b-modal.modal-center5 href="" class="img-responsive">
+              <b-modal id="modal-center5" centered title="BootstrapVue">
+    <p class="my-4">Modal 5!</p>
+  </b-modal>
+      <div class = "correcao" v-b-modal.modal-center6>
+                      <b-modal id="modal-center6" centered title="BootstrapVue">
+    <p class="my-4">Modal 6!</p>
+  </b-modal>
+      <img src= "../assets/Imagem 10.png"  href="" class="img-r1">
+
       </div>
 </div>
 <div class = "proj2">
-      <img src= "../assets/Imagem 3.png"  href="" class="img-responsive">
-      <img src= "../assets/Imagem 5.png"  href="" class="img-r2">
-      <img src= "../assets/Imagem 2.png"  href="" class="img-responsive">
-      <img src= "../assets/Imagem 9.png"  href="" class="img-responsive">
+      <img src= "../assets/Imagem 3.png" v-b-modal.modal-center7 href="" class="img-responsive">
+                            <b-modal id="modal-center7" centered title="BootstrapVue">
+    <p class="my-4">Modal 7!</p>
+  </b-modal>
+      <img src= "../assets/Imagem 5.png" v-b-modal.modal-center8 href="" class="img-r2">
+                            <b-modal id="modal-center8" centered title="BootstrapVue">
+    <p class="my-4">Modal 8!</p>
+  </b-modal>
+      <img src= "../assets/Imagem 2.png" v-b-modal.modal-center9 href="" class="img-responsive">
+                            <b-modal id="modal-center9" centered title="BootstrapVue">
+    <p class="my-4">Modal 9!</p>
+  </b-modal>
+      <img src= "../assets/Imagem 9.png" v-b-modal.modal-center10 href="" class="img-responsive">
+                            <b-modal id="modal-center10" centered title="BootstrapVue">
+    <p class="my-4">Modal 10!</p>
+  </b-modal>
 </div>
 </div>
       </b-tab>
-      <b-tab title="AMBIENTAÇÃO">
+      <b-tab title="AMBIENTAÇÃO⠀⠀⠀⠀⠀⠀" title-link-class ="h4">
       <div class = "nomes"> 
      <div class = "proj">
-      <img src= "../assets/Imagem 8.png"  href="" class="img-responsive">
-      <img src= "../assets/Imagem 4.png"  href="" class="img-responsive">
-      <img src= "../assets/Imagem 6.png"  href="" class="img-responsive">
-      <img src= "../assets/Imagem 7.png"  href="" class="img-responsive">
-      <img src= "../assets/Imagem 11.png" href="" class="img-responsive">
-      <div class = "correcao">
+      <img src= "../assets/Imagem 8.png" v-b-modal.modal-center11 href="" class="img-responsive">
+      <b-modal id="modal-center11" centered title="BootstrapVue">
+    <p class="my-4">Modal 11!</p>
+  </b-modal>
+      <img src= "../assets/Imagem 4.png" v-b-modal.modal-center12 href="" class="img-responsive">
+      <b-modal id="modal-center12" centered title="BootstrapVue">
+    <p class="my-4">Modal 12!</p>
+  </b-modal>
+      <img src= "../assets/Imagem 6.png" v-b-modal.modal-center13 href="" class="img-responsive">
+      <b-modal id="modal-center13" centered title="BootstrapVue">
+    <p class="my-4">Modal 13!</p>
+  </b-modal>
+      <img src= "../assets/Imagem 7.png" v-b-modal.modal-center14 href="" class="img-responsive">
+      <b-modal id="modal-center14" centered title="BootstrapVue">
+    <p class="my-4">Modal 14!</p>
+  </b-modal>
+      <img src= "../assets/Imagem 11.png" v-b-modal.modal-center15 href="" class="img-responsive">
+      <b-modal id="modal-center15" centered title="BootstrapVue">
+    <p class="my-4">Modal 15!</p>
+  </b-modal>
+      <div class = "correcao" v-b-modal.modal-center16>
+        <b-modal id="modal-center16" centered title="BootstrapVue">
+    <p class="my-4">Modal 16!</p>
+  </b-modal>
       <img src= "../assets/Imagem 10.png" href="" class="img-r1">
       </div>
 </div>
 <div class = "proj2">
-      <img src= "../assets/Imagem 3.png"  href="" class="img-responsive">
-      <img src= "../assets/Imagem 5.png"  href="" class="img-r2">
-      <img src= "../assets/Imagem 2.png"  href="" class="img-responsive">
-      <img src= "../assets/Imagem 9.png"  href="" class="img-responsive">
+      <img src= "../assets/Imagem 3.png" v-b-modal.modal-center17 href="" class="img-responsive">
+      <b-modal id="modal-center17" centered title="BootstrapVue">
+    <p class="my-4">Modal 17!</p>
+  </b-modal>
+      <img src= "../assets/Imagem 5.png" v-b-modal.modal-center18 href="" class="img-r2">
+      <b-modal id="modal-center18" centered title="BootstrapVue">
+    <p class="my-4">Modal 18!</p>
+  </b-modal>
+      <img src= "../assets/Imagem 2.png" v-b-modal.modal-center19 href="" class="img-responsive">
+      <b-modal id="modal-center19" centered title="BootstrapVue">
+    <p class="my-4">Modal 19!</p>
+  </b-modal>
+      <img src= "../assets/Imagem 9.png" v-b-modal.modal-center20 href="" class="img-responsive">
+      <b-modal id="modal-center20" centered title="BootstrapVue">
+    <p class="my-4">Modal 20!</p>
+  </b-modal>
 </div>
 </div>
       </b-tab>
-            <b-tab title="EDITORIAL">
-        <div class = "nomes"> 
+            <b-tab title="EDITORIAL ⠀⠀⠀⠀" title-link-class ="h4">
+         <div class = "nomes"> 
      <div class = "proj">
-      <img src= "../assets/Imagem 8.png"  href="" class="img-responsive">
-      <img src= "../assets/Imagem 4.png"  href="" class="img-responsive">
-      <img src= "../assets/Imagem 6.png"  href="" class="img-responsive">
-      <img src= "../assets/Imagem 7.png"  href="" class="img-responsive">
-      <img src= "../assets/Imagem 11.png" href="" class="img-responsive">
-      <div class = "correcao">
+      <img src= "../assets/Imagem 8.png" v-b-modal.modal-center21 href="" class="img-responsive">
+      <b-modal id="modal-center21" centered title="BootstrapVue">
+    <p class="my-4">Modal 21!</p>
+  </b-modal>
+      <img src= "../assets/Imagem 4.png" v-b-modal.modal-center22 href="" class="img-responsive">
+      <b-modal id="modal-center22" centered title="BootstrapVue">
+    <p class="my-4">Modal 22!</p>
+  </b-modal>
+      <img src= "../assets/Imagem 6.png" v-b-modal.modal-center23 href="" class="img-responsive">
+      <b-modal id="modal-center23" centered title="BootstrapVue">
+    <p class="my-4">Modal 23!</p>
+  </b-modal>
+      <img src= "../assets/Imagem 7.png" v-b-modal.modal-center24 href="" class="img-responsive">
+      <b-modal id="modal-center24" centered title="BootstrapVue">
+    <p class="my-4">Modal 24!</p>
+  </b-modal>
+      <img src= "../assets/Imagem 11.png" v-b-modal.modal-center25 href="" class="img-responsive">
+      <b-modal id="modal-center25" centered title="BootstrapVue">
+    <p class="my-4">Modal 25!</p>
+  </b-modal>
+      <div class = "correcao" v-b-modal.modal-center26>
+        <b-modal id="modal-center26" centered title="BootstrapVue">
+    <p class="my-4">Modal 26!</p>
+  </b-modal>
       <img src= "../assets/Imagem 10.png" href="" class="img-r1">
       </div>
 </div>
 <div class = "proj2">
-      <img src= "../assets/Imagem 3.png"  href="" class="img-responsive">
-      <img src= "../assets/Imagem 5.png"  href="" class="img-r2">
-      <img src= "../assets/Imagem 2.png"  href="" class="img-responsive">
-      <img src= "../assets/Imagem 9.png"  href="" class="img-responsive">
+      <img src= "../assets/Imagem 3.png" v-b-modal.modal-center27 href="" class="img-responsive">
+      <b-modal id="modal-center27" centered title="BootstrapVue">
+    <p class="my-4">Modal 27!</p>
+  </b-modal>
+      <img src= "../assets/Imagem 5.png" v-b-modal.modal-center28 href="" class="img-r2">
+      <b-modal id="modal-center28" centered title="BootstrapVue">
+    <p class="my-4">Modal 28!</p>
+  </b-modal>
+      <img src= "../assets/Imagem 2.png" v-b-modal.modal-center29 href="" class="img-responsive">
+      <b-modal id="modal-center29" centered title="BootstrapVue">
+    <p class="my-4">Modal 29!</p>
+  </b-modal>
+      <img src= "../assets/Imagem 9.png" v-b-modal.modal-center30 href="" class="img-responsive">
+      <b-modal id="modal-center30" centered title="BootstrapVue">
+    <p class="my-4">Modal 30!</p>
+  </b-modal>
 </div>
-</div>
-      </b-tab>
-            <b-tab title="LAYOUT DE SITES">
-        <div class = "nomes"> 
+</div></b-tab>
+            <b-tab title="LAYOUT DE SITES" title-link-class ="h4">
+         <div class = "nomes"> 
      <div class = "proj">
-      <img src= "../assets/Imagem 8.png"  href="" class="img-responsive">
-      <img src= "../assets/Imagem 4.png"  href="" class="img-responsive">
-      <img src= "../assets/Imagem 6.png"  href="" class="img-responsive">
-      <img src= "../assets/Imagem 7.png"  href="" class="img-responsive">
-      <img src= "../assets/Imagem 11.png" href="" class="img-responsive">
-      <div class = "correcao">
+      <img src= "../assets/Imagem 8.png" v-b-modal.modal-center31 href="" class="img-responsive">
+      <b-modal id="modal-center31" centered title="BootstrapVue">
+    <p class="my-4">Modal 31!</p>
+  </b-modal>
+      <img src= "../assets/Imagem 4.png" v-b-modal.modal-center32 href="" class="img-responsive">
+      <b-modal id="modal-center32" centered title="BootstrapVue">
+    <p class="my-4">Modal 32!</p>
+  </b-modal>
+      <img src= "../assets/Imagem 6.png" v-b-modal.modal-center33 href="" class="img-responsive">
+      <b-modal id="modal-center33" centered title="BootstrapVue">
+    <p class="my-4">Modal 33!</p>
+  </b-modal>
+      <img src= "../assets/Imagem 7.png" v-b-modal.modal-center34 href="" class="img-responsive">
+      <b-modal id="modal-center34" centered title="BootstrapVue">
+    <p class="my-4">Modal 34!</p>
+  </b-modal>
+      <img src= "../assets/Imagem 11.png" v-b-modal.modal-center35 href="" class="img-responsive">
+      <b-modal id="modal-center35" centered title="BootstrapVue">
+    <p class="my-4">Modal 35!</p>
+  </b-modal>
+      <div class = "correcao" v-b-modal.modal-center36>
+        <b-modal id="modal-center36" centered title="BootstrapVue">
+    <p class="my-4">Modal 36!</p>
+  </b-modal>
       <img src= "../assets/Imagem 10.png" href="" class="img-r1">
       </div>
 </div>
 <div class = "proj2">
-      <img src= "../assets/Imagem 3.png"  href="" class="img-responsive">
-      <img src= "../assets/Imagem 5.png"  href="" class="img-r2">
-      <img src= "../assets/Imagem 2.png"  href="" class="img-responsive">
-      <img src= "../assets/Imagem 9.png"  href="" class="img-responsive">
+      <img src= "../assets/Imagem 3.png" v-b-modal.modal-center37 href="" class="img-responsive">
+      <b-modal id="modal-center37" centered title="BootstrapVue">
+    <p class="my-4">Modal 37!</p>
+  </b-modal>
+      <img src= "../assets/Imagem 5.png" v-b-modal.modal-center38 href="" class="img-r2">
+      <b-modal id="modal-center38" centered title="BootstrapVue">
+    <p class="my-4">Modal 38!</p>
+  </b-modal>
+      <img src= "../assets/Imagem 2.png" v-b-modal.modal-center39 href="" class="img-responsive">
+      <b-modal id="modal-center39" centered title="BootstrapVue">
+    <p class="my-4">Modal 39!</p>
+  </b-modal>
+      <img src= "../assets/Imagem 9.png" v-b-modal.modal-center40 href="" class="img-responsive">
+      <b-modal id="modal-center40" centered title="BootstrapVue">
+    <p class="my-4">Modal 40!</p>
+  </b-modal>
 </div>
-</div>
-      </b-tab>
-            <b-tab title="SINALIZAÇÃO">
-        <div class = "nomes"> 
+</div></b-tab>
+            <b-tab title="⠀⠀⠀⠀⠀⠀⠀SINALIZAÇÃO" title-link-class ="h4">
+         <div class = "nomes"> 
      <div class = "proj">
-      <img src= "../assets/Imagem 8.png"  href="" class="img-responsive">
-      <img src= "../assets/Imagem 4.png"  href="" class="img-responsive">
-      <img src= "../assets/Imagem 6.png"  href="" class="img-responsive">
-      <img src= "../assets/Imagem 7.png"  href="" class="img-responsive">
-      <img src= "../assets/Imagem 11.png" href="" class="img-responsive">
-      <div class = "correcao">
+      <img src= "../assets/Imagem 8.png" v-b-modal.modal-center41 href="" class="img-responsive">
+      <b-modal id="modal-center41" centered title="BootstrapVue">
+    <p class="my-4">Modal 41!</p>
+  </b-modal>
+      <img src= "../assets/Imagem 4.png" v-b-modal.modal-center42 href="" class="img-responsive">
+      <b-modal id="modal-center42" centered title="BootstrapVue">
+    <p class="my-4">Modal 42!</p>
+  </b-modal>
+      <img src= "../assets/Imagem 6.png" v-b-modal.modal-center43 href="" class="img-responsive">
+      <b-modal id="modal-center43" centered title="BootstrapVue">
+    <p class="my-4">Modal 43!</p>
+  </b-modal>
+      <img src= "../assets/Imagem 7.png" v-b-modal.modal-center44 href="" class="img-responsive">
+      <b-modal id="modal-center44" centered title="BootstrapVue">
+    <p class="my-4">Modal 44!</p>
+  </b-modal>
+      <img src= "../assets/Imagem 11.png" v-b-modal.modal-center45 href="" class="img-responsive">
+      <b-modal id="modal-center45" centered title="BootstrapVue">
+    <p class="my-4">Modal 45!</p>
+  </b-modal>
+      <div class = "correcao" v-b-modal.modal-center46>
+        <b-modal id="modal-center46" centered title="BootstrapVue">
+    <p class="my-4">Modal 46!</p>
+  </b-modal>
       <img src= "../assets/Imagem 10.png" href="" class="img-r1">
       </div>
 </div>
 <div class = "proj2">
-      <img src= "../assets/Imagem 3.png"  href="" class="img-responsive">
-      <img src= "../assets/Imagem 5.png"  href="" class="img-r2">
-      <img src= "../assets/Imagem 2.png"  href="" class="img-responsive">
-      <img src= "../assets/Imagem 9.png"  href="" class="img-responsive">
+      <img src= "../assets/Imagem 3.png" v-b-modal.modal-center47 href="" class="img-responsive">
+      <b-modal id="modal-center47" centered title="BootstrapVue">
+    <p class="my-4">Modal 47!</p>
+  </b-modal>
+      <img src= "../assets/Imagem 5.png" v-b-modal.modal-cente48 href="" class="img-r2">
+      <b-modal id="modal-center48" centered title="BootstrapVue">
+    <p class="my-4">Modal 48!</p>
+  </b-modal>
+      <img src= "../assets/Imagem 2.png" v-b-modal.modal-center49 href="" class="img-responsive">
+      <b-modal id="modal-center49" centered title="BootstrapVue">
+    <p class="my-4">Modal 49!</p>
+  </b-modal>
+      <img src= "../assets/Imagem 9.png" v-b-modal.modal-center50 href="" class="img-responsive">
+      <b-modal id="modal-center50" centered title="BootstrapVue">
+    <p class="my-4">Modal 50!</p>
+  </b-modal>
 </div>
-</div>
-      </b-tab>
+</div></b-tab>
     </b-tabs>
   </b-card>
-
-        <!-- <h4>IDENTIDADE VISUAL</h4>
-        <h4>AMBIENTAÇÃO</h4>
-        <h4>EDITORIAL</h4>
-        <h4>LAYOUT DE SITES</h4>
-        <h4>SINALIZAÇÃO</h4> -->
-<!--         
-      <md-tab id="tab1" md-label="IDENTIDADE VISUAL" to="/identidade"></md-tab>
-      <md-tab id="tab2" md-label="AMBIENTAÇÃO" to="/ambientacao"></md-tab>
-      <md-tab id="tab3" md-label="EDITORIAL" to="/editorial"></md-tab>
-      <md-tab id="tab4" md-label="LAYOUT DE SITES" to="/sites"></md-tab>
-      <md-tab id="tab5" md-label="SINALIZAÇÃO" to="/sinalizacao"></md-tab> -->
       </div>
 
 
@@ -142,7 +282,6 @@ export default {
 .card-header{
  border-bottom:0px !important;
   background-color: transparent !important;
-
 }
 
 
@@ -226,7 +365,6 @@ export default {
   align-items: flex-start;
   height: 12.5vw;
   width: 100%;
-  margin-top:10vh;
 }
 
 .proj2{
@@ -278,7 +416,7 @@ h4{
 }
 
 @media screen and (max-width: 1140px) {
-h4{
+h4,a{
   font-size: 17px; 
 }
 
